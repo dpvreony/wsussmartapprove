74dbed4e8afb289003fdf5507b252af00c60d489
David Vreony <dpvreony@users.noreply.github.com>
2020-11-09 21:27:54 +0000

    add wsus back to build.ps1

--- build.ps1
+++ build.ps1
@@ -1,12 +1,14 @@
 $ErrorActionPreference = 'Stop'
 
-#$osInfo = Get-WmiObject -Class Win32_OperatingSystem
-#if ($osInfo.ProductType -eq "Server")
-#{
-	#Install-WindowsFeature UpdateServices-API
-#} else {
-    #Get-WindowsCapability -Name 'RSAT.WSUS.TOOLS*' -Online | Add-WindowsCapability –Online
-#}
+$osInfo = Get-WmiObject -Class Win32_OperatingSystem
+if ($osInfo.ProductType -eq "Server")
+{
+	Install-WindowsFeature UpdateServices-API
+}
+else
+{
+    Get-WindowsCapability -Name 'RSAT.WSUS.TOOLS*' -Online | Add-WindowsCapability -Online
+}
 
 $solutionName = 'WsusSmartApprove'
 $solutionPath = 'src\\' + $solutionName + '.sln'
